2718d766495fd87d36f115619c86486a9cdfa3d6
Your Name <alo20172@uvg.edu.gt>
2022-10-07 19:42:59 -0600

    Proyect 2 added a menu and a printmatrix function

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -74,7 +74,6 @@ void* calculateTranspuesta(void* arg){
 // SECCION DE ADJUNTA 
 void* calculateAdjunta(void* arg)
 {
-    
     Matrix *localM = (Matrix*) arg;
     localM->adj[0][0] = ((localM->matriz[1][1] * localM->matriz[2][2]) - (localM->matriz[2][1] * localM->matriz[1][2]));
     localM->adj[0][1] = -((localM->matriz[1][0] * localM->matriz[2][2]) - (localM->matriz[2][0] * localM->matriz[1][2]));
@@ -91,17 +90,25 @@ void* calculateAdjunta(void* arg)
 } 
 // FIN DE SECCION ADJUNTA
 
-
+// SECCION PARA LA FUNCION IMPRIMIDORA DE MATRICES
+void printMatrix(int matrix[3][3]){
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }  
+    cout<<endl;
+}
 int main()
 {
     sem_init(&determinanteSem, 0, 1);
     pthread_t tDeterminant;
     pthread_t tTranspuest;
     pthread_t tAdjacent;
-    Matrix matrixInput; 
-    int *determinant;
-    Matrix *trans;
-    Matrix *adj;
+    Matrix matrixInput;
     // Ingreso de la matriz
     cout << "Ingrese la matriz de 3x3: \n"; 
     for (int i = 0; i < 3; i++)
@@ -138,37 +145,41 @@ int main()
         perror("Failed to join the thread\n");
         return 2;
     }
-    cout<<"Matriz ingresada:\n";
-    // IMPRESION DE MATRIZ NORMAL
-    for (int i = 0; i < 3; i++)
+    int option;
+    while (true)
     {
-        for (int k = 0; k < 3; k++)
+        cout<<"Seleccione entre las siguientes opciones:\n1-Obtener la matriz ingresada\n2-Obtener el determinante\n3-Obtener la matriz transpuesta\n4-Obtener la matriz adjunta\n5-Obtener la matriz inversa\n6-Salir\n";
+        cin>>option;
+        switch (option)
         {
-            cout << matrixInput.matriz[i][k]<<" ";
+        case 1:
+        // IMPRESION DE MATRIZ INGRESADA
+        cout<<"\nMatriz ingresada:\n";
+        printMatrix(matrixInput.matriz);
+            break;
+        case 2:
+        cout << "Determinante: " << matrixInput.determinant <<endl;
+        break;
+        case 3:
+        // IMPRESION DE MATRIZ TRANSPUESTA
+        cout<<"Matriz transpuesta: \n";
+        printMatrix(matrixInput.transpuest);
+        break;
+        case 4:
+        // IMPRESION DE MATRIZ ADJUNTA
+        cout<<"Matriz adjunta: \n";
+        printMatrix(matrixInput.adj);
+        break;
+        case 5:
+        // IMPRESION DE MATRIZ INVERSA
+        cout<<"Matriz inversa: \n";
+        break;
+        case 6:
+        cout<<"Gracias por utilizar nuestro programa\n";
+        return 0;
+        default:
+        cout<<"No ingreso una opcion valida";
+            break;
         }
-        cout<<endl;
-    } 
-    cout<<endl;
-    cout << "Determinante: " << matrixInput.determinant << "\nMatriz transpuesta: \n";
-    // IMPRESION DE MATRIZ TRANSPUESTA
-    for (int i = 0; i < 3; i++)
-    {
-        for (int k = 0; k < 3; k++)
-        {
-            cout << matrixInput.transpuest[i][k]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<"\nMatriz adjunta: \n";
-    // IMPRESION DE MATRIZ ADJUNTA
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << matrixInput.adj[i][j]<<" ";
-        }
-        cout<<endl;
-    }  
-    
-    return 0;
+    }   
 }
