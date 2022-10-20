6e12d9e5ce4dc9c8987890c2484042dd43ab0fec
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-10-19 18:05:37 -0600

    xd

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -6,40 +6,49 @@
  * Diego Alonzo 20172
  * Daniel Cabrera 20289
  */ 
+
 // Importacion de librerias
 #include <unistd.h>
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
+
+#define NUM_THREADS 10
+
 using namespace std;
 sem_t determinanteSem;
+
 struct Matrix
 {
     int matriz[3][3];
     int determinant = 0;
     int transpuest[3][3];
     int adj[3][3];
-    int inv[3][3];
+    double inv[3][3]; // pasar a double: 1/det * matrix[i][j]
 };
+
 // SECCION DE CALCULO DE DETERMINANTE
 void* determinantFirst(void* arg){
     Matrix localM = *(Matrix*) arg;
-    localM.determinant+= localM.matriz[0][0]*(localM.matriz[1][1]*localM.matriz[2][2]-localM.matriz[2][1]*localM.matriz[1][2]);
+    localM.determinant += localM.matriz[0][0]*(localM.matriz[1][1]*localM.matriz[2][2]-localM.matriz[2][1]*localM.matriz[1][2]);
     sem_wait(&determinanteSem);
     return (void*) arg;
 }
+
 void* determinantSecond(void* arg){
     Matrix localM = *(Matrix*) arg;
     localM.determinant += (-1)*localM.matriz[0][1]*(localM.matriz[1][0]*localM.matriz[2][2]-localM.matriz[2][0]*localM.matriz[1][2]);
     sem_wait(&determinanteSem);
     return (void*) arg; 
 }
+
 void* determinantThird(void* arg){
     Matrix localM = *(Matrix*) arg;
     localM.determinant += localM.matriz[0][2]*(localM.matriz[1][0]*localM.matriz[2][1]-localM.matriz[2][0]*localM.matriz[1][1]);
     sem_post(&determinanteSem);
     return (void*) arg; 
 }
+
 void* calculateDeterminant(void* arg){
     Matrix localM = *(Matrix*) arg;
     pthread_t first;
@@ -91,6 +100,36 @@ void* calculateAdjunta(void* arg)
 } 
 // FIN DE SECCION ADJUNTA
 
+// CALCULO DE MATRIZ INVERSA
+void* calculateInverse(void* args)
+{
+    Matrix *localM = (Matrix*) args;
+
+    // // determinante temporal
+    localM->determinant += localM->matriz[0][0]*(localM->matriz[1][1]*localM->matriz[2][2]-localM->matriz[2][1]*localM->matriz[1][2]);
+    localM->determinant += (-1)*localM->matriz[0][1]*(localM->matriz[1][0]*localM->matriz[2][2]-localM->matriz[2][0]*localM->matriz[1][2]);
+    localM->determinant += localM->matriz[0][2]*(localM->matriz[1][0]*localM->matriz[2][1]-localM->matriz[2][0]*localM->matriz[1][1]);
+
+    cout << "\nDET = " << localM->determinant << "\n";
+    
+    if (localM->determinant != 0)
+    {
+        for (int i = 0 ; i < 3; i++ )
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                localM->inv[i][j] = (float) (1/localM->determinant)*(localM->adj[i][j]);
+                
+            }
+            
+        }
+    }
+
+    return NULL;
+
+}
+// FIN DE SECCION INVERSA
+
 // SECCION PARA LA FUNCION IMPRIMIDORA DE MATRICES
 void printMatrix(int matrix[3][3]){
     for (int i = 0; i < 3; i++)
@@ -103,13 +142,31 @@ void printMatrix(int matrix[3][3]){
     }  
     cout<<endl;
 }
+
+// SECCION PARA LA FUNCION IMPRIMIDORA DE MATRICES double
+void printMatrixDouble(double matrix[3][3]){
+    cout.precision(4);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }  
+    cout<<endl;
+}
+
+// MAIN
 int main()
 {
     sem_init(&determinanteSem, 0, 1);
     pthread_t tDeterminant;
     pthread_t tTranspuest;
     pthread_t tAdjacent;
+    pthread_t tInverse;
     Matrix matrixInput;
+
     // Ingreso de la matriz
     cout << "Ingrese la matriz de 3x3: \n"; 
     for (int i = 0; i < 3; i++)
@@ -134,6 +191,12 @@ int main()
            perror("Failed to create the thread\n");
            return 1;
     }
+    if  (pthread_create(&tInverse, NULL, &calculateInverse, (void*)&matrixInput)!=0){ //In case creating threads fail.
+           perror("Failed to create the thread\n");
+           return 1;
+    }
+
+
     if (pthread_join( tDeterminant, NULL ) != 0 ){ //In case the join fails
         perror("Failed to join the thread\n");
         return 2;
@@ -146,40 +209,55 @@ int main()
         perror("Failed to join the thread\n");
         return 2;
     }
+    if (pthread_join( tInverse, NULL ) != 0 ){ //In case the join fails
+        perror("Failed to join the thread\n");
+        return 2;
+    }
+
     int option;
+    
     while (true)
     {
         cout<<"Seleccione entre las siguientes opciones:\n1-Obtener la matriz ingresada\n2-Obtener el determinante\n3-Obtener la matriz transpuesta\n4-Obtener la matriz adjunta\n5-Obtener la matriz inversa\n6-Salir\n";
         cin>>option;
+
         switch (option)
         {
-        case 1:
-        // IMPRESION DE MATRIZ INGRESADA
-        cout<<"\nMatriz ingresada:\n";
-        printMatrix(matrixInput.matriz);
+            case 1:
+            // IMPRESION DE MATRIZ INGRESADA
+            cout<<"\nMatriz ingresada:\n";
+            printMatrix(matrixInput.matriz);
             break;
-        case 2:
-        cout << "Determinante: " << matrixInput.determinant <<endl;
-        break;
-        case 3:
-        // IMPRESION DE MATRIZ TRANSPUESTA
-        cout<<"Matriz transpuesta: \n";
-        printMatrix(matrixInput.transpuest);
-        break;
-        case 4:
-        // IMPRESION DE MATRIZ ADJUNTA
-        cout<<"Matriz adjunta: \n";
-        printMatrix(matrixInput.adj);
-        break;
-        case 5:
-        // IMPRESION DE MATRIZ INVERSA
-        cout<<"Matriz inversa: \n";
-        break;
-        case 6:
-        cout<<"Gracias por utilizar nuestro programa\n";
-        return 0;
-        default:
-        cout<<"No ingreso una opcion valida";
+
+            case 2:
+            // IMPRESION DEL DET
+            cout << "Determinante: " << matrixInput.determinant <<endl;
+            break;
+
+            case 3:
+            // IMPRESION DE MATRIZ TRANSPUESTA
+            cout<<"Matriz transpuesta: \n";
+            printMatrix(matrixInput.transpuest);
+            break;
+
+            case 4:
+            // IMPRESION DE MATRIZ ADJUNTA
+            cout<<"Matriz adjunta: \n";
+            printMatrix(matrixInput.adj);
+            break;
+
+            case 5:
+            // IMPRESION DE MATRIZ INVERSA
+            cout<<"Matriz inversa: \n";
+            printMatrixDouble(matrixInput.inv);
+            break;
+
+            case 6:
+            cout<<"Gracias por utilizar nuestro programa\n";
+            return 0;
+
+            default:
+            cout<<"No ingreso una opcion valida";
             break;
         }
     }   
